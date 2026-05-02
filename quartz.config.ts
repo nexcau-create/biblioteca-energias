b4c1192bca9a6bff914ec492d8e37c5ce27126d3
Nexcau <nexcau@gmail.com>
2026-05-01 22:47:39 -0300

    corrige baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "nexcau-create.github.io/biblioteca-energias",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
